19495929448d2f885dc6401d33246ad0790f1aad
FieryFoxed <122393543+FieryFoxed@users.noreply.github.com>
2023-10-02 21:34:25 -0400

    Commit 12 : Developed UI advance,ents

--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -1,16 +1,17 @@
 package edu.bsu.cs222;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-
 
 public class Main extends Application{
     public Label resultLabel;
@@ -30,42 +31,26 @@ public class Main extends Application{
 
         String information = changeLogFormatter.changeLogToString(changeLog.jsonInfo(input));
         String redirectInfo = formatRedirect.redirectFormat(changeLog.jsonInfo(input));
-        String together = information + " " + redirectInfo;
+        String together = information + "\n\n" + redirectInfo;
 
         return together;
     }
 
     @Override
     public void start(Stage primaryStage){
-
         TextField searchField = new TextField();
         searchField.setPromptText("What Wikipedia article are you looking for?: ");
         Button searchButton = new Button("Search");
-
-        searchButton.setOnAction(event -> {
-            String userInput = searchField.getText();
-            String errorCheck = userError.getSearch(userInput);
-
-            if (errorCheck == "No input!") {
-                resultLabel.setText("Search results for: " + userInput + "\n" + errorCheck);
-            } else {
-                String together = combine(userInput);
-                resultLabel.setText("Search results for: " + userInput + "\n" + together);
-            }
-
-
-
-
-        });
-
         resultLabel = new Label();
 
+        HBox hbox = new HBox(10);
+        hbox.getChildren().addAll(searchField, searchButton);
+        hbox.setAlignment(Pos.TOP_CENTER);
+
         VBox vbox = new VBox(20);
-        vbox.getChildren().addAll(searchField, searchButton, resultLabel);
-
+        vbox.getChildren().addAll(hbox, resultLabel);
         Scene scenes = new Scene(vbox);
-
-
+        vbox.setAlignment(Pos.CENTER);
 
         Screen screen = Screen.getPrimary();
         Rectangle2D bounds = screen.getVisualBounds();
@@ -77,5 +62,17 @@ public class Main extends Application{
 
         primaryStage.setScene(scenes);
         primaryStage.show();
+
+        searchButton.setOnAction(event -> {
+            String userInput = searchField.getText();
+            String errorCheck = userError.getSearch(userInput);
+
+            if (errorCheck == "No input!") {
+                resultLabel.setText("Changelogs for : " + userInput + "\n\n" + errorCheck);
+            } else {
+                String together = combine(userInput);
+                resultLabel.setText("Changelogs for : " + userInput + "\n\n" + together);
+            }
+        });
     }
 }
--- src/main/java/edu/bsu/cs222/User.java
+++ src/main/java/edu/bsu/cs222/User.java
@@ -2,15 +2,12 @@ package edu.bsu.cs222;
 
 public class User {
     public String getSearch(String userInput){
-        String errorCheck = blankCheck(userInput);
-        return errorCheck;
+        return blankCheck(userInput);
     }
     private String blankCheck(String userInput){
         if (userInput.equals("")){
-            throw new Error("No input!");
             return "No input!";
         } if (userInput.trim().equals("")){
-            throw new Error("No input!");
             return "No input!";
         }
         return userInput;
