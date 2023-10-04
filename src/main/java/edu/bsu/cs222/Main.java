4b8648c0a16c650ee341e278ff3f60a0735cd88e
FieryFoxed <122393543+FieryFoxed@users.noreply.github.com>
2023-10-03 21:34:06 -0400

    Commit 14 : Finalized UI and cleanup

--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -33,70 +33,64 @@ public class Main extends Application {
 
         String information = changeLogFormatter.changeLogToString(changeLog.jsonInfo(input));
         String redirectInfo = formatRedirect.redirectFormat(changeLog.jsonInfo(input));
+
+
         return information + "\n\n" + redirectInfo;
     }
     @Override
     public void start(Stage primaryStage) {
-        try {
-            TextField searchField = new TextField();
-            searchField.setPromptText("Enter an article: ");
-            Button searchButton = new Button("Search");
-            resultLabel = new Label();
+        TextField searchField = new TextField();
+        searchField.setPromptText("Enter an article: ");
+        Button searchButton = new Button("Search");
+        resultLabel = new Label();
 
-            HBox hbox = new HBox(10);
-            hbox.getChildren().addAll(searchField, searchButton);
-            hbox.setAlignment(Pos.TOP_CENTER);
+        HBox hbox = new HBox(10);
+        hbox.getChildren().addAll(searchField, searchButton);
+        hbox.setAlignment(Pos.TOP_CENTER);
 
-            VBox vbox = new VBox(20);
-            vbox.getChildren().addAll(hbox, resultLabel);
-            Scene scenes = new Scene(vbox);
-            vbox.setAlignment(Pos.CENTER);
+        VBox vbox = new VBox(20);
+        vbox.getChildren().addAll(hbox, resultLabel);
+        Scene scenes = new Scene(vbox);
+        vbox.setAlignment(Pos.CENTER);
 
-            Screen screen = Screen.getPrimary();
-            Rectangle2D bounds = screen.getVisualBounds();
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
 
-            primaryStage.setX(bounds.getMinX());
-            primaryStage.setY(bounds.getMinY());
-            primaryStage.setWidth(bounds.getWidth());
-            primaryStage.setHeight(bounds.getHeight());
+        primaryStage.setX(bounds.getMinX());
+        primaryStage.setY(bounds.getMinY());
+        primaryStage.setWidth(bounds.getWidth());
+        primaryStage.setHeight(bounds.getHeight());
 
-            primaryStage.setScene(scenes);
-            primaryStage.show();
+        primaryStage.setScene(scenes);
+        primaryStage.show();
 
-            searchButton.setOnAction(event -> {
-                String userInput = searchField.getText();
-                String errorCheck = userError.checkSearch(userInput);
+        searchButton.setOnAction(event -> {
+            String userInput = searchField.getText();
+            String errorCheck = userError.checkSearch(userInput);
 
-                if (errorCheck.equals("No input!")) {
-                    Stage inputError = new Stage();
-                    inputError.initModality(Modality.APPLICATION_MODAL);
-                    inputError.initStyle(StageStyle.UTILITY);
-                    inputError.setTitle("User input error");
+            if (errorCheck.equals("No input!")) {
+                Stage inputError = new Stage();
+                inputError.initModality(Modality.APPLICATION_MODAL);
+                inputError.initStyle(StageStyle.UTILITY);
+                inputError.setTitle("User input error");
 
-                    Label label = new Label("No user input!");
-                    Button closeButton = new Button("Close");
-                    closeButton.setOnAction(e -> inputError.close());
+                Label label = new Label("No user input!");
+                Button closeButton = new Button("Close");
+                closeButton.setOnAction(e -> inputError.close());
 
-                    StackPane dialogLayout = new StackPane();
-                    VBox LackOfInput = new VBox(20);
-                    LackOfInput.getChildren().addAll(label, closeButton);
-                    LackOfInput.setAlignment(Pos.CENTER);
-                    dialogLayout.getChildren().addAll(LackOfInput);
-                    Scene dialogScene = new Scene(dialogLayout, 200, 100);
-                    inputError.setScene(dialogScene);
+                StackPane dialogLayout = new StackPane();
+                VBox LackOfInput = new VBox(20);
+                LackOfInput.getChildren().addAll(label, closeButton);
+                LackOfInput.setAlignment(Pos.CENTER);
+                dialogLayout.getChildren().addAll(LackOfInput);
+                Scene dialogScene = new Scene(dialogLayout, 200, 100);
+                inputError.setScene(dialogScene);
 
-                    inputError.showAndWait();
-                } else {
-                    String together = combine(userInput);
-                    resultLabel.setText("Changelogs for : " + userInput + "\n\n" + together);
-                }
-            });
-        } catch (RuntimeException ioException) {
-            //do error stuff
-            System.exit(0);
-        } catch (Error error) {
-            //do error stuff?
-            System.exit(0);
-        }
+                inputError.showAndWait();
+            } else {
+                String together = combine(userInput);
+                resultLabel.setText("Changelogs for : " + userInput + "\n\n" + together);
+            }
+        });
     }
 }
